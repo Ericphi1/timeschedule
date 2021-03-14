c9ca8d07ea86ce4ed82c0ba5ea7edd5934f85aab
Ericphi1 <80648759+Ericphi1@users.noreply.github.com>
2021-03-14 21:54:46 +0100

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,22 @@
+class App extends StatefulWidget {
+  // This widget is the root of your application.
+  @override
+  _AppState createState() => _AppState();
+}
+
+
+class _AppState extends State<App> {
+  void initState() {
+    _loadData();
+    super.initState();
+  }
+
+  _loadData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if(prefs.getString("todo") != null){ 
+    time = jsonDecode(prefs.getString("time"));
+  }}
+  Map<String, dynamic> time = {};
+Map<String, dynamic> who = {};
+Map<String, dynamic> when = {};
+Map<String, dynamic> where = {};
